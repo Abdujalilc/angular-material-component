da7f82174b979a42d3a173c8e292d4673a4cff92
abdujalilc <abdujalilc@gmail.com>
2022-06-26 17:18:29 +0500

    folder name changes

--- src/app/components/25_sidenav/03_sidenav-drawer-overview-example/sidenav-drawer-overview-example.ts/sidenav-drawer-overview-example.ts
+++ src/app/components/25_sidenav/03_sidenav-drawer-overview-example/sidenav-drawer-overview-example.ts/sidenav-drawer-overview-example.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 /** @title Basic drawer */
 @Component({
   selector: 'sidenav-drawer-overview-example',
-  templateUrl: './sidenav-drawer-overview-example.html',
+  templateUrl: 'sidenav-drawer-overview-example.html',
   styleUrls: ['sidenav-drawer-overview-example.css'],
 })
 export class SidenavDrawerOverviewExample {}
